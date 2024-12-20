1589f6758754fb5bf572e8fe8d60f83d05426a00
LKW07 <86255001+LKW07@users.noreply.github.com>
2024-12-20 11:55:27 +0100

    Add 3 Gewinnt

---
+++ 3 Gewinnt - Mit KI/3 Gewinnt/change_p.h
@@ -0,0 +1,6 @@
+#ifndef CHANGE_P_H
+#define CHANGE_P_H
+
+int change_p(int);
+
+#endif
---
+++ 3 Gewinnt - Mit KI/3 Gewinnt/ki_move.h
@@ -0,0 +1,6 @@
+#ifndef KI_MOVE_H
+#define KI_MOVE_H
+
+void ki_move(int, int, int, int, char[][6]);
+
+#endif
