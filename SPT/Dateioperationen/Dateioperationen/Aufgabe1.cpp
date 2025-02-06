8bb6a962be874109622f288322156a34a4eddde6
LKW07 <weiss.leander@steinweiss.de>
2025-02-06 08:57:02 +0100

    added Dateioperationen

---
+++ SPT/Dateioperationen/Dateioperationen/Aufgabe1.cpp
@@ -0,0 +1,16 @@
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void main() { // Bei Benutzung umbenennen in "main"!
+	char input[10];
+
+	cout << "Bitte geben Sie den Dateinamen ein (mit '.txt' Endung): ";
+	cin >> input;
+	FILE* f = fopen(input, "w");
+	if (f == NULL) cout << "ERRROR!! Die Datei konnte nicht erstellt werden" << endl;
+	else {
+		cout << endl << "Die Datei " << input << " wurde erfolgreich erstellt" << endl;
+	}
+}
---
+++ SPT/Dateioperationen/Dateioperationen/Aufgabe2.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void main() {
+	//TODO
+}
