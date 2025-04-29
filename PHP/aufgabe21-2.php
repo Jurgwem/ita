c63701017013111c3fc2a688a697935fd18dc27f
Jurgwem <n.bresi05@gmail.com>
2025-04-29 09:26:16 +0200

    ay caramba ep2

--- PHP/aufgabe21-2.php
+++ PHP/aufgabe21-2.php
@@ -1,12 +1,12 @@
 <html><head><title></title></head><body>
-<h1>21.1 - QR-Code Telefonnummer</h1>
+<h1>21.2 - Grundgesetz</h1>
 <?php
     $text = file("data/gg.txt");
 
     include("qrlib.php");
 
     QRcode::png(implode($text), "temp/gg.png", "H", 10, 2);  
-    //Shit doesnt work because tooooo big
+    //doesnt work because tooo big
     echo '<img src="temp/gg.png">'; 
 
     echo "<br><br><br>";
